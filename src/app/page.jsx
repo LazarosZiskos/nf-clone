cc205872cdb190e1949e233cbfb9e00a6c1b4bc6
LazarosZiskos <ziskoslaz@gmail.com>
2024-03-08 20:30:42 +0200

    implemented row and thumbnail components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,23 @@
 import Banner from "@/components/Banner";
 import Row from "@/components/Row";
-import { getTrending } from "@/constants/requests";
+import {
+  getTrending,
+  getTopRated,
+  getAction,
+  getComedies,
+  getHorror,
+  getRomance,
+  getDocumentaries,
+} from "@/constants/requests";
 
 export default async function Home() {
   const trending = await getTrending();
+  const topRated = await getTopRated();
+  const action = await getAction();
+  const comedies = await getComedies();
+  const scary = await getHorror();
+  const romance = await getRomance();
+  const documentaries = await getDocumentaries();
 
   return (
     <main
@@ -14,7 +28,13 @@ export default async function Home() {
         <Banner />
       </section>
       <section className="md:space-y-24 pl-4 lg:pl-16">
-        <Row movies={trending} title="Trending" />
+        <Row movies={trending} title="Trending Now" />
+        <Row movies={topRated} title="Top Rated" />
+        <Row movies={action} title="Action Thrillers" />
+        <Row movies={comedies} title="Comedies" />
+        <Row movies={scary} title="Scary Movies" />
+        <Row movies={romance} title="Romance Movies" />
+        <Row movies={documentaries} title="Documentaries" />
       </section>
     </main>
   );
--- src/constants/requests.js
+++ src/constants/requests.js
@@ -10,3 +10,52 @@ export const getTrending = async () => {
   const data = await res.json();
   return data.results;
 };
+
+export const getTopRated = async () => {
+  const res = await fetch(
+    `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`
+  );
+
+  const top = await res.json();
+  return top.results;
+};
+
+export const getAction = async () => {
+  const res = await fetch(
+    `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`
+  );
+  const action = await res.json();
+  return action.results;
+};
+
+export const getComedies = async () => {
+  const res = await fetch(
+    `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=35`
+  );
+  const comedy = await res.json();
+  return comedy.results;
+};
+
+export const getHorror = async () => {
+  const res = await fetch(
+    `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=27`
+  );
+  const horror = await res.json();
+  return horror.results;
+};
+
+export const getRomance = async () => {
+  const res = await fetch(
+    `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=10749`
+  );
+  const romance = await res.json();
+  return romance.results;
+};
+
+export const getDocumentaries = async () => {
+  const res = await fetch(
+    `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=99`
+  );
+  const documentaries = await res.json();
+  return documentaries.results;
+};
