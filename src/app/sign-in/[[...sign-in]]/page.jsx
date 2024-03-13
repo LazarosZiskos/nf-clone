f080137670005b892c0be028145cdae37de2f7e8
LazarosZiskos <ziskoslaz@gmail.com>
2024-03-13 19:33:41 +0200

    implemented Clerk Authentication

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -1,3 +1,108 @@
-export default function Page() {
-  return <SignIn />;
+"use client";
+import { useState } from "react";
+import { useSignIn } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function SignInForm() {
+  const { isLoaded, signIn, setActive } = useSignIn();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  // start the sign In process.
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const result = await signIn.create({
+        identifier: email,
+        password,
+      });
+
+      if (result.status === "complete") {
+        console.log(result);
+        await setActive({ session: result.createdSessionId });
+        router.push("/");
+      } else {
+        /*Investigate why the sign-in hasn't completed */
+        console.log(result);
+      }
+    } catch (err) {
+      console.error("error", err.errors[0].longMessage);
+    }
+  };
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div className="relative flex h-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Image
+        src="https://rb.gy/p2hphi"
+        layout="fill"
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        objectFit="cover"
+        alt="background-image"
+      />
+
+      <img
+        src="https://rb.gy/ulxxee"
+        className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+        width={150}
+        height={150}
+      />
+      <form
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
+      md:max-w-md md:px-14"
+      >
+        <h1 className="text-4xl font-semibold">Sign Up</h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="email"
+              className="input"
+              onChange={emailHandler}
+            />
+          </label>
+          <label className="inline-block w-full">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="password"
+              className="input"
+              onChange={passwordHandler}
+            />
+          </label>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+          onClick={handleSubmit}
+        >
+          Sign In
+        </button>
+
+        <div className="text-[gray]">
+          New to Netflix?{" "}
+          <Link href="/sign-up" className="text-white hover:underline">
+            Sign up now
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -1,16 +1,161 @@
 "use client";
-import { useSignUp } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import Link from "next/link";
+import { useSignUp } from "@clerk/nextjs";
+import Image from "next/image";
 
-export default function Page() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [code, setCode] = useState();
-  const { isLoaded, signUp, setActive } = useSignUp();
-
-  const handleSubmit = () => {};
-
+const signUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [pendingVerification, setPendingVerification] = useState(false);
+  const [code, setCode] = useState("");
   const router = useRouter();
-  return <div>sign-up page</div>;
-}
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      await signUp.create({
+        email_address: email,
+        password,
+      });
+
+      // send the email.
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      // change the UI to our pending section.
+      setPendingVerification(true);
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  // This verifies the user using email code that is delivered.
+  const onPressVerify = async (e) => {
+    e.preventDefault();
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const completeSignUp = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+      if (completeSignUp.status !== "complete") {
+        /*  investigate the response, to see if there was an error
+         or if the user needs to complete more steps.*/
+        console.log(JSON.stringify(completeSignUp, null, 2));
+      }
+      if (completeSignUp.status === "complete") {
+        await setActive({ session: completeSignUp.createdSessionId });
+        router.push("/");
+      }
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  };
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+  };
+  return (
+    <div className="relative flex h-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
+      <Image
+        src="https://rb.gy/p2hphi"
+        layout="fill"
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        objectFit="cover"
+        alt="background-image"
+      />
+
+      <img
+        src="https://rb.gy/ulxxee"
+        className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
+        width={150}
+        height={150}
+      />
+      {!pendingVerification && (
+        <form
+          className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
+      md:max-w-md md:px-14"
+        >
+          <h1 className="text-4xl font-semibold">Sign Up</h1>
+          <div className="space-y-4">
+            <label className="inline-block w-full">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="email"
+                className="input"
+                onChange={emailHandler}
+              />
+            </label>
+            <label className="inline-block w-full">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="password"
+                className="input"
+                onChange={passwordHandler}
+              />
+            </label>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full rounded bg-[#e50914] py-3 font-semibold"
+            onClick={handleSubmit}
+          >
+            Sign Up
+          </button>
+
+          <div className="text-[gray]">
+            Already have an account?{" "}
+            <Link href="/login" className="text-white hover:underline">
+              Sign in now
+            </Link>
+          </div>
+        </form>
+      )}
+
+      {pendingVerification && (
+        <form
+          className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
+      md:max-w-md md:px-14"
+        >
+          <h1 className="text-4xl font-semibold">Verification</h1>
+          <div className="space-y-4">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Verification Code"
+              className="input"
+              onChange={(e) => setCode(e.target.value)}
+            />
+
+            <button
+              type="submit"
+              className="w-full rounded bg-[#e50914] py-3 font-semibold"
+              onClick={onPressVerify}
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default signUp;
